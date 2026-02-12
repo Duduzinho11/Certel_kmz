efba6713a4a74d0f55be09362698ff235772a489
Eduardo Tiggemann <141690546+Duduzinho11@users.noreply.github.com>
2026-02-12 10:45:38 -0300

    Add files via upload

---
+++ site/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'certel-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.10.0/localforage.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => {
+    if (k !== CACHE_NAME) return caches.delete(k);
+  }))));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
